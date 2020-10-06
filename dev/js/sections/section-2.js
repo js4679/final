d083dc0c195e279fbf95eaa49c3999d7f31ff30e
js4679 <63320565+js4679@users.noreply.github.com>
2020-10-06 15:42:20 -0400

    section-2 scrolltrigger

--- dev/js/sections/section-2.js
+++ dev/js/sections/section-2.js
@@ -6,7 +6,7 @@ gsap.registerPlugin(ScrollTrigger);
 
 const section2TL = gsap.timeline();
 section2TL.from("#specials h1",{duration:0.5,alpha:0,x:300})
-        .from("#contact-us hu",{duration:0.5,alpha:0,x:-300});
+        .from("#contact-us h1",{duration:0.5,alpha:0,x:-300});
 
 
 export function section2Animation(){
