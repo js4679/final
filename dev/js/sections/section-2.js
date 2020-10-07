fe33ca3c5bf401f45e51b3a401b57e9a9f7bb3c3
js4679 <63320565+js4679@users.noreply.github.com>
2020-10-06 21:06:56 -0400

    after-inspect

--- dev/js/sections/section-2.js
+++ dev/js/sections/section-2.js
@@ -5,8 +5,8 @@ gsap.registerPlugin(ScrollTrigger);
 
 
 const section2TL = gsap.timeline();
-section2TL.from("#specials h1",{duration:1, alpha:0, y:100})
-        .from("#contact-us h1",{duration:1, alpha:0, y:-100});
+section2TL.from("#specials h1",{duration:.5, alpha:0, y:100})
+        .from("#contact-us h1",{duration:.5, alpha:0, y:-100});
 
 
 export function section2Animation(){
@@ -15,7 +15,7 @@ export function section2Animation(){
         markers: true,
         scrub: true,
         animation: section2TL,
-        toggleActions: "restart none reverse none",
+        toggleActions: "play",
         trigger: "#section-2",
         start:"top",
         end: "bottom"
